3ac0f0755dcf4136f49d2e293995050c95f4ab3a
sumavk96 <sumavkumar96@gmail.com>
2024-09-21 00:12:21 +0530

    Delete src directory

--- src/constants/constants.js
+++
@@ -1,4 +0,0 @@
-export const baseUrl='https://api.themoviedb.org/3';
-export const API_KEY='39757ce98d3c998361ea4702ec55c363';
-export const imageUrl ='https://image.tmdb.org/t/p/original';
-// export const baseUrl='';
